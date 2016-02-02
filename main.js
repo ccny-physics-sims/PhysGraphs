e310f334e01a0feb066e403e10765eb0cce02db2
aareman <aareman000@citymail.cuny.edu>
2016-02-02 09:12:16 -0500

    Basic frame drawing completed
    can draw a scalable background with axis and labeling.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function setup(){
 	createCanvas(800,500);
+	test = new Graph(400,400,0,20,0,20,10);
+	test.showBorder = true;
+	test.x_offset = 20;
+	test.y_offset = 20;
 }
 function draw(){
-	//just some filler code while the graph object is being developed.
-	background(0);
-	fill(255);
-	ellipse(20,20,40,40);
+	//test code
+	background(100);	
+	test.drawBg();
 }
--- physgraphs.js
+++ physgraphs.js
@@ -32,7 +32,7 @@ function Graph(w, h, x_min, x_max, y_min, y_max, resoloution){
 	// initial variables
 	this.width = w;
 	this.height = h;
-	this.res = resoloution;
+	this.resoloution = resoloution;
 	this.x_min = x_min;
 	this.x_max = x_max;
 	this.y_min = y_min;
@@ -46,6 +46,8 @@ function Graph(w, h, x_min, x_max, y_min, y_max, resoloution){
 	this.showBorder = true;
 	this.borderWidth = 2;
 	
+	this.x_offset = 0;
+	this.y_offset = 0;
 	
 	//functional variables
 	this.plots = [];
@@ -57,6 +59,68 @@ Graph.prototype.update = function(){
 };
 //draw the axis, labels, etc... (the graph without the curves)
 Graph.prototype.drawBg = function(){
+		
+	//border
+	if(this.showBorder == false){
+		noStroke();
+		//line(this.x_offset, this.y_offset, this.width, this.y_offset);
+		//line(this.x_offset, this.y_offset, this.x_offset, this.height);
+		//line(this.x_offset, this.height, this.width, this.height);
+		//line(this.width, this.y_offset, this.width, this.height);
+	}else{
+		stroke(0);
+	}
+	
+	//set background color of graph
+	fill(255);
+	
+	//draw base layer of graph
+	rect(this.x_offset, this.y_offset, this.width, this.height);
+	
+	//draw axis
+	stroke(0);
+	line(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
+			this.x_offset+this.width*0.95, this.y_offset+this.height*0.9) //x border
+	line(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
+			this.x_offset+this.width*0.1, this.y_offset+this.height*0.05) //y border
+	
+	//compute resoloution values
+	var xdiff = (this.x_max - this.x_min)/this.resoloution;
+	var ydiff = (this.y_max - this.y_min)/this.resoloution;
+	
+	//compute pixel resoloution
+	var xpix = dist(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
+					this.x_offset+this.width*0.95, this.y_offset+this.height*0.9)/this.resoloution;
+	var ypix = dist(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
+					this.x_offset+this.width*0.1, this.y_offset+this.height*0.05)/this.resoloution;
+	
+	//draw x values and vertical lines
+	fill(0);
+	stroke(180);
+	var count = this.x_min; //for counting intermediary values
+	var pixCount = 0;
+		//draw zero	
+		text(this.x_min, this.x_offset+this.width*0.1, this.y_offset+this.height*0.9+20);
+	for(var i = 0; i < this.resoloution; i++){
+		count += xdiff;
+		pixCount += xpix;
+		line(this.x_offset+this.width*0.1 + pixCount, this.y_offset+this.height*0.9+5, 
+				this.x_offset+this.width*0.1 + pixCount, this.y_offset+this.height*0.05);
+		text(Math.round(count).toString(), this.x_offset+this.width*0.1 + pixCount, this.y_offset+this.height*0.9+20);		
+	}
+	
+	//draw x values and vertical lines
+	var count = this.y_min; //for counting intermediary values
+	var pixCount = 0;
+		//draw zero	
+		text(this.y_min, this.x_offset+this.width*0.1 - 20, this.y_offset+this.height*0.9);
+	for(var i = 0; i < this.resoloution; i++){
+		count += ydiff;
+		pixCount += ypix;
+		line(this.x_offset+this.width*0.1 - 5, this.y_offset+this.height*0.9-pixCount, 
+				this.x_offset+this.width*0.95, this.y_offset+this.height*0.9-pixCount);
+		text(Math.round(count).toString(), this.x_offset+this.width*0.1 - 20, this.y_offset+this.height*0.9-pixCount);		
+	}
 	
 };
 //plots all plots on this graph
