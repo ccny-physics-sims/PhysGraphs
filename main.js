1e8c511373ea195992d5cb7c94525f29957166f4
aareman <aareman000@citymail.cuny.edu>
2016-02-22 16:29:51 -0500

    some stuff

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function setup(){
 	createCanvas(800,500);
-	test = new Graph(400,400,0,10,0,10,10);
+	test = new Graph(400,400,-2,2,-2,2,2);
 	test.showBorder = true;
 	test.x_offset = 20;
 	test.y_offset = 20;
 	
-	var x = [0,1,2,3,4,5,6,7,8,9,10,12,15,16];
-	var y = [0,2,4,6,8,10,8,6,4,3,2,2,2,2];
+	var x = [-1,1,2,3,4,5,6,7,8,9,10,12,15,16,17,18];
+	var y = [0,2,4,6,8,10,8,6,4,3,2,2,2,2,4,5];
 	
 	pArray = Graph.makeData(x,y);
 	
@@ -14,16 +14,16 @@ function setup(){
 	
 	plot1 = new Plot(pArray,167,203,237,3);
 	
-	p1 = new Point(0,0);
-	p2 = new Point(1,1);
-	p3 = new Point(2,4);
-	p4 = new Point(3,9);
+	p1 = new Point(-1,-2);
+	p2 = new Point(0,0);
+	p3 = new Point(1,0);
+	p4 = new Point(2,0);
 	
 	pArray = [p1,p2,p3,p4];
 	
 	plot2 = new Plot(pArray,255,0,0,2);
 	
-	test.addPlot(plot1);
+	//test.addPlot(plot1);
 	test.addPlot(plot2);
 }
 function draw(){
@@ -31,7 +31,9 @@ function draw(){
 	background(100);	
 	test.drawBg();
 	test.plotAll();
+	
+	 //ellipse(test.origin.x,test.origin.y,20,20);
 }
 function mouseDragged(){
-	test.plots[0].getUser();
+	//test.plots[0].getUser();
 }
--- physgraphs.js
+++ physgraphs.js
@@ -1,6 +1,4 @@
 ﻿/*ToDo:
- * -add function to graph class to generate an array of points from
- * seperate arrays of x and y choords [DONE]
  * -add equation class
  * -add user interativity for mouse [DONE]and touch [In progress...]
  * -fix scaling problems
@@ -8,7 +6,6 @@
  * 		-with numbers and edging distances
  * -add a legend to the graph
  * -add other kinds of lines to draw the plot with
- * -simplify into a scale coords function. [DONE]
  * -trim out of bounds points and lines
  * */
 
@@ -18,11 +15,28 @@ function Point(x,y){
 	this.x = x;
 	this.y = y;
 }
+Point.prototype.add = function(x,y){
+	this.x += x;
+	this.y += y;
+};
 //fixes the y choordinates to work with p5.js
 Point.prototype.fixChoord = function(){
 	if(this.y > 0){
 		this.y = this.y*-1; //inverts the y axis so that positive is up not down
 	}	
+};
+//inverts both choordinates
+Point.prototype.invert = function(){
+	this.y *= -1;
+	this.x *= -1;
+};
+//convert choord to pixel position
+Point.prototype.makePix = function(xoff, yoff, scalex, scaley){
+	
+	this.fixChoord();
+	this.x = this.x*scalex+xoff;
+	this.y = this.y*scaley+yoff;
+	
 };
 /*
  * input = takes in two point objects
@@ -34,6 +48,10 @@ Point.getDist = function(p1,p2){
 	pFinal.x = Math.abs(p1.x-p2.x); //gets the x distance between points
 	pFinal.y = Math.abs(p1.y-p2.y); //gets the y distance between points
 	return pFinal;
+};
+Point.getPoint = function(p){
+	var point = new Point(p.x,p.y);
+	return point;
 };
 //a function object for graphing functions
 function Equation(){
@@ -49,7 +67,7 @@ function Plot(pointArray, red, green, blue, weight){
 	this.weight = weight; // a number for the stroke thickness of the graph
 }
 //regular plotting function
-Plot.prototype.plot = function(){
+Plot.prototype.plot = function(graph){
 	//drawMode("center"); <--need to check p5.js for proper syntax
 	
 	//set up the styles for what will be drawn
@@ -60,13 +78,16 @@ Plot.prototype.plot = function(){
 	//draws the data points and the connecting lines
 	for(var i = 0;i<this.data.length;i++){
 		
-		if(i<this.data.length-1){
+		//if(i<this.data.length-1 && this.data[i+1].x <= graph.width && this.data[i+1].y <= graph.height){
 			//draws the connecting lines, scaling the data so that it corresponds to our coordinate space
 			line(this.data[i].x, this.data[i].y,
 					this.data[i+1].x, this.data[i+1].y); 
-		}
+		//}
 		//draws the data points, with scaling and offset.
-		ellipse(this.data[i].x, this.data[i].y, 8, 8);
+		//if(this.data[i].x <= graph.width && this.data[i+1].y <= graph.height){
+			ellipse(this.data[i].x, this.data[i].y, 8, 8);	
+		//}
+		
 	}
 };
 
@@ -92,13 +113,16 @@ Plot.prototype.getPointDist = function(num1, num2){
 	return dist(this.data[num1].x, this.data[num1].y, 
 				this.data[num2].x, this.data[num2].y);
 };
-Plot.prototype.fixChoord = function(xoff, yoff, scalex, scaley){
+Plot.prototype.fixChoord = function(xoff, yoff, scalex, scaley, origin){
+	var p = Point.getPoint(origin);
 	for(var i = 0;i<this.data.length;i++){
 		this.data[i].fixChoord();
-		this.data[i].x = this.data[i].x*scalex+xoff;
-		this.data[i].y = this.data[i].y*scaley+yoff;
+		p.add(this.data[i].x*scalex, this.data[i].y*scaley);
+		this.data[i].x = p.x;
+		this.data[i].y = p.y;
+		p = Point.getPoint(origin);
 	}
-}
+};
 
 function Graph(w, h, x_min, x_max, y_min, y_max, resoloution){
 	
@@ -126,16 +150,29 @@ function Graph(w, h, x_min, x_max, y_min, y_max, resoloution){
 	
 	//functional variables
 	this.plots = [];
-	this.xpix = dist(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
-					this.x_offset+this.width*0.95, this.y_offset+this.height*0.9)/this.resoloution; //xScale
-	this.ypix = dist(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
-					this.x_offset+this.width*0.1, this.y_offset+this.height*0.05)/this.resoloution; //yScale
+	
+	/* bl = bottom left.
+	 * this is used for determining correct positioning of graph coordinates.
+	 * */				
+	this.bl_pix = new Point(this.x_offset+this.width*0.1,
+								this.y_offset+this.height*0.9);
+	this.bl_val = new Point(this.x_min, this.y_min);
+	this.bl_val.invert();
+	
+	
+	this.xpix = dist(this.bl_pix.x, this.bl_pix.y, 
+					this.x_offset+this.width*0.95, this.bl_pix.y)/this.resoloution; //xScale in pixels
+	this.ypix = dist(this.bl_pix.x, this.bl_pix.y, 
+					this.bl_pix.x, this.y_offset+this.height*0.05)/this.resoloution; //yScale in pixels
 					
-	this.xunit = dist(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
-					this.x_offset+this.width*0.95, this.y_offset+this.height*0.9)/(this.x_max-this.x_min); //xUnitScale
-	this.yunit = dist(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
-					this.x_offset+this.width*0.1, this.y_offset+this.height*0.05)/(this.y_max-this.y_min); //yUnitScale
-					
+	this.xunit = dist(this.bl_pix.x, this.bl_pix.y, 
+					this.x_offset+this.width*0.95, this.bl_pix.y)/(this.x_max-this.x_min); //xUnitScale 
+	this.yunit = dist(this.bl_pix.x, this.bl_pix.y, 
+					this.bl_pix.x, this.y_offset+this.height*0.05)/(this.y_max-this.y_min); //yUnitScale
+	
+	this.origin = Point.getPoint(this.bl_pix);
+	this.origin.add(this.bl_val.x*this.xunit, this.bl_val.y*this.yunit);
+	console.log(this.origin.x, this.origin.y);
 	
 		
 }
@@ -164,10 +201,10 @@ Graph.prototype.drawBg = function(){
 	
 	//draw axis
 	stroke(0);
-	line(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
-			this.x_offset+this.width*0.95, this.y_offset+this.height*0.9) //x border
-	line(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, 
-			this.x_offset+this.width*0.1, this.y_offset+this.height*0.05) //y border
+	line(this.bl_pix.x, this.bl_pix.y, 
+			this.x_offset+this.width*0.95, this.bl_pix.y) //x border
+	line(this.bl_pix.x, this.bl_pix.y, 
+			this.bl_pix.x, this.y_offset+this.height*0.05) //y border
 	
 	//compute resoloution values
 	var xdiff = (this.x_max - this.x_min)/this.resoloution;
@@ -183,39 +220,52 @@ Graph.prototype.drawBg = function(){
 	var count = this.x_min; //for counting intermediary values
 	var pixCount = 0;
 		//draw zero	
-		text(this.x_min, this.x_offset+this.width*0.1, this.y_offset+this.height*0.9+20);
+		text(this.x_min, this.bl_pix.x, this.bl_pix.y+20);
 	for(var i = 0; i < this.resoloution; i++){
 		count += xdiff;
 		pixCount += xpix;
-		line(this.x_offset+this.width*0.1 + pixCount, this.y_offset+this.height*0.9+5, 
-				this.x_offset+this.width*0.1 + pixCount, this.y_offset+this.height*0.05);
-		text(Math.round(count).toString(), this.x_offset+this.width*0.1 + pixCount, this.y_offset+this.height*0.9+20);		
+		line(this.bl_pix.x + pixCount, this.bl_pix.y+5, 
+				this.bl_pix.x + pixCount, this.y_offset+this.height*0.05);
+		text((Math.round(10*count)/10).toString(), this.bl_pix.x + pixCount, this.bl_pix.y+20);		
 	}
 	
-	//draw x values and vertical lines
+	//draw y values and vertical lines
 	var count = this.y_min; //for counting intermediary values
 	var pixCount = 0;
 		//draw zero	
-		text(this.y_min, this.x_offset+this.width*0.1 - 20, this.y_offset+this.height*0.9);
+		text(this.y_min, this.bl_pix.x - 20, this.bl_pix.y);
 	for(var i = 0; i < this.resoloution; i++){
 		count += ydiff;
 		pixCount += ypix;
-		line(this.x_offset+this.width*0.1 - 5, this.y_offset+this.height*0.9-pixCount, 
-				this.x_offset+this.width*0.95, this.y_offset+this.height*0.9-pixCount);
-		text(Math.round(count).toString(), this.x_offset+this.width*0.1 - 20, this.y_offset+this.height*0.9-pixCount);		
+		line(this.bl_pix.x - 5, this.bl_pix.y-pixCount, 
+				this.x_offset+this.width*0.95, this.bl_pix.y-pixCount);
+		text((Math.round(10*count)/10).toString(), this.bl_pix.x - 20, this.bl_pix.y-pixCount);		
 	}
 	
 };
 //plots all plots on this graph
 Graph.prototype.plotAll = function(){
 	for(var i = 0; i<this.plots.length;i++){		
-		this.plots[i].plot();		
+		this.plots[i].plot(this);		
 	}
 };
 //add a new plot to the graph.
 Graph.prototype.addPlot = function(aplot){
-	aplot.fixChoord(this.x_offset+this.width*0.1, this.y_offset+this.height*0.9, this.xunit, this.yunit);
+	//var temp = this.getZeroes();
+	aplot.fixChoord(this.bl_pix.x, 
+						this.bl_pix.y, 
+						this.xunit, 
+						this.yunit,
+						this.origin);
 	this.plots.push(aplot);
+};
+//get the x and y zero.
+Graph.prototype.getZeroes = function(){
+	
+};
+//get pixel x and y values for a point in the graph.
+Graph.prototype.getChoord = function(x, y){
+	
 };
 
 //this might make more sense to put as part of the Plot object
